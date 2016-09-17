2aa29b86441ccf0766e86ad2e480215cc8848598
JYOTIRMAY SENAPATI <jyotirmaysenapati@JYOTIRMAYs-MacBook-Pro.local>
2016-09-17 23:15:04 +0530

    commit

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express');
+var compress = require('compression');
+var favicon = require('serve-favicon');
+//var cluster = require('cluster');
+var path = require('path');
+
+var app = express();
+app.use(compress());
+app.use(favicon(path.join(__dirname, 'public', 'images', 'myblog.ico')));
+app.use(express.static(__dirname + "/public/build"));
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+
