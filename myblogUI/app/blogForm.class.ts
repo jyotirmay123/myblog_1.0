7ad25096eb7f0cf95a25c7e7b68e1757a42b1c77
System Administrator <root@JYOTIRMAYs-MacBook-Pro.local>
2016-10-10 22:14:48 +0530

    Initial Commit_5

--- myblogUI/app/blogForm.class.ts
+++
@@ -1,9 +0,0 @@
-/*export
-class blogFormModule {
-    constructor(public blogDate: number,
-                public blogContent: string,
-                public blogger: string,
-                public blogId: number) {
-        
-    }
-}*/
